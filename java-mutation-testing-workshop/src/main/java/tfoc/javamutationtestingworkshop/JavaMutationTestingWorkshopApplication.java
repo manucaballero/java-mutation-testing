f8cab31a6a5652714edffa9eba4935083a37a638
Manuel Caballero Rabago <mcabalra@mercadona.es>
2024-02-26 11:43:43 +0100

    feat: Initialized project

---
+++ java-mutation-testing-workshop/src/main/java/tfoc/javamutationtestingworkshop/JavaMutationTestingWorkshopApplication.java
@@ -0,0 +1,13 @@
+package tfoc.javamutationtestingworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMutationTestingWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMutationTestingWorkshopApplication.class, args);
+    }
+
+}
---
+++ java-mutation-testing-workshop/src/test/java/tfoc/javamutationtestingworkshop/JavaMutationTestingWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package tfoc.javamutationtestingworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMutationTestingWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
